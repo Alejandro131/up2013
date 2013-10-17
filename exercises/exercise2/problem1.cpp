bd3470df70cde5d2ab4df56a5f60cc10466a7c08
George Shopov <gsshopov@gmail.com>
2013-10-17 15:28:14 +0300

    Add problem1 for exercise2

---
+++ exercises/exercise2/problem1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Използваме unsigned int, защото изминалото време в секунди е положително
+    // число.
+    unsigned int pastSeconds;
+
+    cout << "Enter past seconds: ";
+    cin >> pastSeconds;
+
+    unsigned int minutes = pastSeconds / 60;
+    unsigned int hours = minutes / 60;
+
+    hours %= 24;
+    minutes %= 60;
+
+    cout.fill('0');  // Ако останат празни места от 'setw' ще бъдат запълнени с '0'
+    cout << "\nThe time is " << setw(2) << hours << ":"
+         << setw(2) << minutes << endl;
+
+    return 0;
+}
