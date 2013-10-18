dfcae698e46b1edf9b9be7257e321048dc90118b
George Shopov <gsshopov@gmail.com>
2013-10-18 13:59:24 +0300

    Add problem 6 from exercise 2

---
+++ exercises/exercise2/problme6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double radius;
+
+    cout << "Enter the radius: ";
+    cin >> radius;
+
+    double pointX, pointY;
+
+    cout << "Enter the 'x' and 'y' coordinates of the point: ";
+    cin >> pointX >> pointY;
+
+    // Уравнението на окръжността е:
+    // (x - ox)*(x - ox) + (y - oy)*(y - oy) = radius*radius, където (ox, oy)
+    // е центърът на окръжността, който в момента е (0, 0). Следователно
+    // уравнението придобива вида: x*x + y*y = radius*radius.
+    cout << boolalpha << (pointX*pointX + pointY*pointY <= radius*radius)
+         << endl;
+
+    return 0;
+}
